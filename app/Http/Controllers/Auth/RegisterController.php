48193c706d93d4fc2fce0b67f2253a8a5be14738
Danilo <daniloworkdev@gmail.com>
2022-05-28 20:55:47 +0100

    feat(TarefaController): implementando método auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:4', 'confirmed'], // confirmed relaciona outro campo do form,
+            'password' => ['required', 'string', 'min:4', 'confirmed'], // confirmed relaciona outro campo do form
         ]);
     }
 
