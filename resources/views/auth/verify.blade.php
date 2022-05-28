566f22950ea9bda5d5754578808ae84d7c2b8e00
Danilo <daniloworkdev@gmail.com>
2022-05-28 23:32:43 +0100

    feat(view verificação e-mail): traduzindo conteúdo da pagina

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Falta pouco agora! Precisamos apenas que você valide seu e-mail') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Renviamos um e-mail para você com um link de validação!') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Atens de utilizar os recursos da aplicação, por favor valide seu e-mail por meio de link de verificação que encaminhamos por email!') }}
+                    {{ __('Clique no link a seguir para receber um novo email!') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Clique aqui!') }}</button>.
                     </form>
                 </div>
             </div>
