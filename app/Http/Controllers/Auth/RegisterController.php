831a5ebad83a9e0a93899b4da13e20d22c855c3b
Danilo <daniloworkdev@gmail.com>
2022-05-28 20:30:05 +0100

    feat(Register): traduzindo conteúdo da pagina

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -22,7 +22,7 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+    use RegistersUsers; //trait funciona como um control-c control-v funciona como um (include)
 
     /**
      * Where to redirect users after registration.
