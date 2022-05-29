9ac92ff62d79aa18f27b414d93d1240594cb7e63
Danilo <daniloworkdev@gmail.com>
2022-05-29 21:43:59 +0100

    feat: validação de tarefas do usuário, segurança

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -107,7 +107,13 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        return view('tarefa.edit', ['tarefa' => $tarefa]);
+        $user_id = auth()->user()->id;
+        if($tarefa->user_id == $user_id){
+            return view('tarefa.edit', ['tarefa' => $tarefa]);
+        } else{
+            return view('acesso-negado');
+        }
+        
     }
 
     /**
@@ -119,8 +125,13 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
+        if(!$tarefa->user_id == auth()->user()->id){
+            return view('acesso-negado');
+        }
+
         $tarefa->update($request->all());
         return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
+       
     }
 
     /**
