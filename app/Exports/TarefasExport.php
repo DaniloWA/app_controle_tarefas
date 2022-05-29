2f275d33ffa2be4d6f877bd6f66e8de723dbdb80
Danilo <daniloworkdev@gmail.com>
2022-05-29 23:17:20 +0100

    refactor: os usuários só conseguem baixar as próprias tarefas

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -12,6 +12,8 @@ class TarefasExport implements FromCollection
     */
     public function collection()
     {
-        return Tarefa::all();
+        //return Tarefa::all();
+
+        return auth()->user()->tarefas()->get();
     }
 }
