326eb0c0ecb188f23dc48a5db4015eee02b75cab
Danilo <daniloworkdev@gmail.com>
2022-05-30 12:58:40 +0100

    feat: exportação de tarefas em PDF

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -152,15 +152,12 @@ class TarefaController extends Controller
         return redirect()->route('tarefa.index');
     }
 
-    public function exportacao($extensao){                                   //CSV
-        $nome_arquivo = 'lista_de_tarefas';
-        if($extensao == 'xlsx'){
-            $nome_arquivo .= '.'.$extensao;
-        }else if($extensao == 'csv'){
-            $nome_arquivo .= '.'.$extensao;
-        }else{
-            return redirect()->route('tarefa.index');
-        };
-        return Excel::download(new TarefasExport, $nome_arquivo );
+    public function exportacao($extensao){                                  
+
+        if(in_array($extensao, ['xlsx', 'csv', 'pdf'])){
+            return Excel::download(new TarefasExport, 'lista_de_tarefas.'.$extensao );
+        }
+
+        return redirect()->route('tarefa.index');
     }
 }
--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -14,7 +14,8 @@
                       <div class="float-right">
                         <a href="{{ route('tarefa.create') }}" class="mr-3" >Novo</a>
                         <a href="{{ route('tarefa.exportacao', ['extensao' => 'xlsx']) }}" class="mr-3">XLSX</a>
-                        <a href="{{ route('tarefa.exportacao', ['extensao' => 'csv']) }}" >CSV</a>
+                        <a href="{{ route('tarefa.exportacao', ['extensao' => 'csv']) }}" class="mr-3">CSV</a>
+                        <a href="{{ route('tarefa.exportacao', ['extensao' => 'pdf']) }}" >PDF</a>
                       </div>
                     </div>
                   </div>
